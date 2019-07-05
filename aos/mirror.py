b3f30fe1145a060ac35fe2c3d032af7217214a48
David Thomas <davidthomas5412@gmail.com>
2019-07-05 12:21:12 -0700

    add mirror

---
+++ aos/mirror.py
@@ -0,0 +1,142 @@
+import numpy as np
+
+
+class SurfaceResidual:
+    """
+    Class for representing and controlling mirror surface residuals.
+    """
+    def applyForce(self, force):
+        # if len(force) != self.nActuators:
+        #     raise ValueError('Force must have the same length as the number of actuators: {}'
+        #                      .format(self.nActuators))
+        # self.surfResidual += np.dot(self.influenceMatrix, force)
+        # Note: reformat influence matrix to batoid grid
+        raise NotImplementedError()
+
+    def applyBending(self, deltaModes):
+        """
+        Updates surface residual based on bending modes.
+
+        Parameters
+        ----------
+        modes: numpy.ndarray
+            size of the bending modes, in meters.
+        """
+        if len(deltaModes) != self.nModes:
+            raise ValueError('Modes must have the same length as the number of bending modes: {}'
+                             .format(self.nModes))
+        pert = self.bendingMatrix * deltaModes.reshape(self.nModes, 1, 1)
+        self.surfResidual += np.sum(pert, axis=0)
+
+    def applyThermal(self, temps):
+        raise NotImplementedError()
+
+    def applyGravity(self, zenith):
+        raise NotImplementedError()
+
+
+class M1M3Residual(SurfaceResidual):
+    """
+    M1M3 surface residuals.
+
+    Parameters
+    ----------
+    modes: numpy.ndarray
+        bending modes; defaults to None.
+
+    forces: numpy.ndarray
+        actuator forces; defaults to None.
+
+    nModes: int
+        number of modes to use; defaults to None.
+
+
+    Attributes
+    ----------
+    x: numpy.ndarray
+        The 1D 'x' axes grid points for the surface.
+    y: numpy.ndarray
+        The 1D 'y' axes grid points for the surface.
+    bendingMatrix: numpy.ndarray
+        The matrix that maps modes to the surface grid.
+    nModes: int
+        The number of bending modes to use.
+    nActuators: int
+        The number of actuators to use.
+    surfResidual:
+        The 2D grid of 'z' values corresponding to the grid of x,y.
+    """
+    def __init__(self, modes=None, forces=None, nModes=None):
+        super().__init__()
+        self.x = np.load('../data/M1M3_grid_x.npy')
+        self.y = np.load('../data/M1M3_grid_y.npy')
+        self.bendingMatrix = np.load('../data/M1M3_bending_modes.npy')
+        if nModes is None:
+            self.nModes = self.bendingMatrix.shape[0]
+        else:
+            self.bendingMatrix = self.bendingMatrix[:nModes]
+            self.nModes = nModes
+
+        self.nActuators = 256
+        nx = len(self.x)
+        ny = len(self.y)
+        self.surfResidual = np.zeros((nx, ny))
+
+        if modes is not None:
+            self.setBending(modes)
+
+        if forces is not None:
+            self.applyForce(forces)
+
+
+class M2Residual(SurfaceResidual):
+    """
+    M2 surface residuals.
+
+    Parameters
+    ----------
+    modes: numpy.ndarray
+        bending modes; defaults to None.
+
+    forces: numpy.ndarray
+        actuator forces; defaults to None.
+
+    nModes: int
+        number of modes to use; defaults to None.
+
+
+    Attributes
+    ----------
+    x: numpy.ndarray
+        The 1D 'x' axes grid points for the surface.
+    y: numpy.ndarray
+        The 1D 'y' axes grid points for the surface.
+    bendingMatrix: numpy.ndarray
+        The matrix that maps modes to the surface grid.
+    nModes: int
+        The number of bending modes to use.
+    nActuators: int
+        The number of actuators to use.
+    surfResidual:
+        The 2D grid of 'z' values corresponding to the grid of x,y.
+    """
+    def __init__(self, modes=None, forces=None, nModes=None):
+        super().__init__()
+        self.x = np.load('../data/M2_grid_x.npy')
+        self.y = np.load('../data/M2_grid_y.npy')
+        self.bendingMatrix = np.load('../data/M2_bending_modes.npy')
+        if nModes is None:
+            self.nModes = self.bendingMatrix.shape[0]
+        else:
+            self.bendingMatrix = self.bendingMatrix[:nModes]
+            self.nModes = nModes
+        self.nActuators = 256
+        nx = len(self.x)
+        ny = len(self.y)
+        self.surfResidual = np.zeros((nx, ny))
+
+        if modes is not None:
+            self.setBending(modes)
+
+        if forces is not None:
+            self.applyForce(forces)
---
+++ tests/test_mirror.py
@@ -0,0 +1,49 @@
+import pytest
+import numpy as np
+from aos.mirror import SurfaceResidual, M1M3Residual, M2Residual
+
+
+def test_nmodes():
+    nModes = 10
+    m1m3residual = M1M3Residual(nModes=nModes)
+    m2residual = M2Residual(nModes=nModes)
+    m1m3residual.applyBending(np.zeros(nModes))
+    m2residual.applyBending(np.zeros(nModes))
+
+    with pytest.raises(ValueError):
+        m1m3residual.applyBending(np.zeros(nModes+1))
+
+    with pytest.raises(ValueError):
+        m2residual.applyBending(np.zeros(nModes + 1))
+
+
+def test_apply_bending_modes():
+    nModes = 2
+    modes = np.zeros(nModes)
+    ind = 1
+    modes[ind] = 1
+    m1m3residual = M1M3Residual(nModes=nModes)
+    m2residual = M2Residual(nModes=nModes)
+    m1m3residual.applyBending(modes)
+    m2residual.applyBending(modes)
+
+    np.testing.assert_allclose(m1m3residual.surfResidual, m1m3residual.bendingMatrix[ind])
+    np.testing.assert_allclose(m2residual.surfResidual, m2residual.bendingMatrix[ind])
+
+
+def test_bending_modes_accumulate():
+    nModes = 2
+    up1 = np.zeros(nModes)
+    up2 = np.zeros(nModes)
+    up1[0] = 1
+    up2[1] = 1
+    comb = np.ones(nModes)
+
+    m1m3res = M1M3Residual(nModes=nModes)
+    m1m3rescomb = M1M3Residual(nModes=nModes)
+
+    m1m3res.applyBending(up1)
+    m1m3res.applyBending(up2)
+    m1m3rescomb.applyBending(comb)
+
+    np.testing.assert_allclose(m1m3res.surfResidual, m1m3rescomb.surfResidual)
