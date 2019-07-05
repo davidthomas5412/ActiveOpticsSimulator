5a6eff324aba373dcf560e194b0da91a0926878f
David Thomas <davidthomas5412@gmail.com>
2019-07-05 14:50:06 -0700

    adding data directory to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ setup(
     author_email='davidthomas5412@gmail.com',
     url='https://github.com/davidthomas5412/ActiveOpticsSimulator',
     description="A Simulator for LSST Active Optics System",
-    packages = ['aos']
+    packages = ['aos'],
+    package_data = {'aos': ['../data/*']},
+    install_requires=['numpy', 'scipy', 'pyyaml'],
+    python_requires='>=3.4',
+    tests_require=['pytest', 'pytest-cov', 'coverage', 'codecov']
 )
