01f3504ef5c9635206ea9a8d1147ab78912000f1
David Thomas <davidthomas5412@gmail.com>
2019-09-05 14:14:25 -0700

    trying to fix donut simulator test

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -16,18 +16,20 @@ def test_opd_simulator():
 
     ref = np.load(os.path.join(aos.testDir, 'nominal_opd_0_0.npy'))
 
-    np.testing.assert_allclose(array, ref, rtol=1e-3)
+    np.testing.assert_allclose(array, ref)
 
 
 def test_donut_simulator():
     np.random.seed(0)
     tel = Telescope.nominal()
+    optic = tel.optic
+    optic = optic.withGloballyShiftedOptic('LSST.LSSTCamera.Detector', [0, 0, -1.5e-3])
     sim = DonutSimulator()
     fieldx = 0
     fieldy = 0
-    donut = sim.simulate(tel.optic, fieldx, fieldy)
+    donut = sim.simulate(optic, fieldx, fieldy)
     array = np.array(donut.array)
 
     ref = np.load(os.path.join(aos.testDir, 'nominal_donut_0_0.npy'))
 
-    np.testing.assert_allclose(array, ref, rtol=1e-3)
+    np.testing.assert_allclose(array, ref)
