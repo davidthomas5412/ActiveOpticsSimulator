7335981737775b33f25311460bd21d00fb3a3327
David Thomas <davidthomas5412@gmail.com>
2019-09-06 18:19:41 -0700

    OPD -> wavefront diction, now simulateWavefront and simulateDonut methods are explicit

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,21 +1,37 @@
 import os
 import aos
+import pytest
 import numpy as np
 from aos.telescope import BendingTelescope
-from aos.simulator import DonutSimulator, OPDSimulator
+from aos.simulator import DonutSimulator, WavefrontSimulator
 
 
-def test_opd_simulator():
+def test_wavefront_simulator():
+    """
+    import os
+    import aos
+    import numpy as np
+    from aos.telescope import BendingTelescope
+    from aos.simulator import WavefrontSimulator
+
     tel = BendingTelescope.nominal()
-    sim = OPDSimulator()
-    fieldx = 0
-    fieldy = 0
-    opd = sim.simulate(tel.optic, fieldx, fieldy)
-    array = np.array(opd.array)
+    sim = WavefrontSimulator()
+    fieldx, fieldy = (0, 0)
+    wavefront = sim.simulateWavefront(tel.optic, fieldx, fieldy)
+    ref = np.save(os.path.join(aos.testDir, 'nominal_wavefront_0_0.npy'), wavefront)
+    """
+    tel = BendingTelescope.nominal()
+    sim = WavefrontSimulator()
+    fieldx, fieldy = (0, 0)
+    wavefront = sim.simulateWavefront(tel.optic, fieldx, fieldy)
+    ref = np.load(os.path.join(aos.testDir, 'nominal_wavefront_0_0.npy'))
 
-    ref = np.load(os.path.join(aos.testDir, 'nominal_opd_0_0.npy'))
+    np.testing.assert_allclose(wavefront, ref)
 
-    np.testing.assert_allclose(array, ref)
+
+def test_even_wavefront_raises():
+    with pytest.raises(ValueError):
+        WavefrontSimulator(nx=250)
 
 
 def test_donut_simulator():
@@ -23,26 +39,21 @@ def test_donut_simulator():
     import os
     import aos
     import numpy as np
-    from aos.telescope import Telescope
-    from aos.simulator import DonutSimulator, OPDSimulator
+    from aos.telescope import BendingTelescope
+    from aos.simulator import DonutSimulator
+    import matplotlib.pyplot as plt
 
-    np.random.seed(0)
-    tel = Telescope.nominal()
-    optic = tel.optic
-    optic = optic.withGloballyShiftedOptic('LSST.LSSTCamera.Detector', [0, 0, -1.5e-3])
+    tel = BendingTelescope.nominal()
     sim = DonutSimulator()
-    fieldx = 0
-    fieldy = 0
-    donut = sim.simulate(optic, fieldx, fieldy)
-    donut = sim.simulate(optic, fieldx, fieldy)
+    fieldx, fieldy = (0, 0)
+    donut = sim.simulateDonut(tel.intra, fieldx, fieldy)
     array = np.array(donut.array)
     np.save(os.path.join(aos.testDir, 'nominal_donut_0_0.npy'), array)
     """
     tel = BendingTelescope.nominal()
     sim = DonutSimulator()
-    fieldx = 0
-    fieldy = 0
-    donut = sim.simulate(tel.intra, fieldx, fieldy)
+    fieldx, fieldy = (0, 0)
+    donut = sim.simulateDonut(tel.intra, fieldx, fieldy)
     array = np.array(donut.array)
 
     ref = np.load(os.path.join(aos.testDir, 'nominal_donut_0_0.npy'))
@@ -51,3 +62,8 @@ def test_donut_simulator():
     poisson_err = np.sum(np.sqrt(ref))
 
     assert err < 2 * poisson_err
+
+
+def test_odd_crop_raises():
+    with pytest.raises(ValueError):
+        DonutSimulator(crop=191)
