76a9111f598b07adde4bf42021099b8e24448052
David Thomas <davidthomas5412@gmail.com>
2019-09-06 18:20:49 -0700

    refactored estimator, now has estimate and evaluate methods

--- aos/estimator.py
+++ aos/estimator.py
@@ -1,51 +1,89 @@
 import numpy as np
-from galsim.zernike import zernikeBasis
+from galsim.zernike import zernikeBasis, Zernike
 
 
-class OPDEstimator:
+class WavefrontEstimator:
     """
-    Class to estimator the wavefront zernike coefficients from OPD images.
+    Class to estimate the wavefront zernike coefficients from wavefront images.
 
     Parameters
     ----------
-    obscuration: float
-        obscuration in optical system; defaults to 0.61 (LSST).
-    nZern: int
-        number of zernikes to use in the wavefront estimate; defaults to 22.
+    outRadius: float
+        outer radius of entrance pupil in meters; defaults to 4.18 (LSST).
+    inRadius: float
+        inner radius of entrance pupil in meters; defaults to 2.558 (LSST).
 
     Attributes
     ----------
-    obscuration: float
-        obscuration in optical system; defaults to 0.61 (LSST).
-    nZern: int
-        number of zernikes to use in the wavefront estimate; defaults to 22.
+    outRadius: float
+        outer radius of entrance pupil in meters; defaults to 4.18 (LSST).
+    inRadius: float
+        inner radius of entrance pupil in meters; defaults to 2.558 (LSST).
     """
-    def __init__(self, obscuration=0.61, nZern=22):
-        self.obscuration = 0.61
-        self.nZern = nZern
+    def __init__(self, outRadius=4.18, inRadius=2.558):
+        self.outRadius = outRadius
+        self.inRadius = inRadius
 
-    def estimate(self, opd):
+    def estimate(self, wavefront, nZern=22):
         """
-        Fits zernikes to opd image.
+        Fits zernikes to wavefront image.
+
+        Notes
+        -----
+        The coefficients are indexed by the Noll (1976) convention, which starts at j=1. The 0th
+        coefficient has no impact.
 
         Parameters
         ----------
-        opd: batoid.Lattice
-            opd image.
+        wavefront: numpy.ndarray
+            wavefront image.
+
+        nZern: int
+            number of coefficients to fit.
 
         Returns
         -------
         numpy.ndarray
-            the zernike coeficients.
+            the zernike coeficients (Noll .
         """
-        nx = opd.array.shape[0]
-        X, Y = np.meshgrid(np.linspace(-1, 1, nx), np.linspace(-1, 1, nx))
-        mask = ~opd.array.mask
-        basis = zernikeBasis(self.nZern, X[mask].flatten(), Y[mask].flatten(),
-                             R_inner=self.obscuration)
-        coefs, _, _, _ = np.linalg.lstsq(basis.T, opd.array[mask].flatten())
-        coefs = np.array(coefs[1:])
+        nx = wavefront.shape[0]
+        space = np.linspace(-self.outRadius, self.outRadius, nx)
+        X, Y = np.meshgrid(space, space)
+        mask = ~np.isnan(wavefront)
+        basis = zernikeBasis(nZern, X[mask].flatten(), Y[mask].flatten(),
+                             R_inner=self.inRadius, R_outer=self.outRadius)
+        coefs, _, _, _ = np.linalg.lstsq(basis.T, wavefront[mask].flatten())
         return coefs
+
+    def evaluate(self, coefs, nx=255):
+        """
+        Produce wavefront image from zernike coefficients.
+
+        Notes
+        -----
+        The coefficients are indexed by the Noll (1976) convention, which starts at j=1. The 0th
+        coefficient has no impact.
+
+        Parameters
+        ----------
+        coefs: numpy.ndarray
+            zernike coefficients.
+        nx: int
+            number of pixels in each dimension; defaults to 255.
+
+        Returns
+        -------
+        numpy.ndarray
+            image of wavefront.
+        """
+        space = np.linspace(-self.outRadius, self.outRadius, nx)
+        X, Y = np.meshgrid(space, space)
+        R = np.sqrt(X ** 2 + Y ** 2)
+        mask = np.logical_and(R <= self.outRadius, R >= self.inRadius)
+        zern = Zernike(coefs, R_inner=self.inRadius, R_outer=self.outRadius)
+        img = zern.evalCartesian(X, Y)
+        img[~mask] = np.nan
+        return img
 
 
 class InversionEstimator:
